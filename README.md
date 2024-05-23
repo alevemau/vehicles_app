a8702c4562136b5f9d9352fcebdb8e652a696a1d
alevemau <alevem09@gmail.com>
2024-05-22 21:11:33 -0600

    app first update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # vehicles_app
 
-Proyecto Sprint 5 vehicles_app
+!Bienvenido!
+
+En esta aplicacción encontrarás información para encontrar tu próximo vehículo, aquí te presentamos datos para que elijas tu favorito, como precio, año del modelo, modelo, el estado en el que se encuentra, cuantos cilindros tiene, que combustible utiliza, la distancia recorrida por el auto, el tipo de auto, el color del auto, además te presentamos gráficos para que puedas comparar entre tus primeras opciones.
---
+++ notebooks/app.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+vehicle_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+df = pd.DataFrame(vehicle_data)
+
+st.title('Vehicle Sales app')
+
+st.dataframe(df)
+
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(vehicle_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+hist_button_d = st.checkbox(
+    'Construir gráfico de dispersión')  # crear un botón
+
+if hist_button_d:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig_d = px.scatter(vehicle_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_d, use_container_width=True)
+
+st.subheader('Condicion con base en el tipo')
+
+fig_bar = px.bar(vehicle_data, color='type',
+                 x='condition')
+st.plotly_chart(fig_bar, use_container_width=True)
+
+st.subheader('Condición vs Año del modelo')
+
+fig_hist = px.histogram(vehicle_data, color='condition',
+                        x='model_year')
+st.plotly_chart(fig_hist, use_container_width=True)
